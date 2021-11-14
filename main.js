fac3abeeeeb7cd277f317b3b1b6b212febf37085
Ehsan shahid <93027825+Ehsan-sh787@users.noreply.github.com>
2021-11-14 14:14:44 +0500

    update blog code

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
- // there is nothing here ;)
+// this little Jquery code is needed here ;)
+$(".menu-toggle").click(function () {
+	$(".menu-toggle").toggleClass('open');
+  $(".menu-round").toggleClass('open');
+	$(".menu-line").toggleClass('open');
+});
